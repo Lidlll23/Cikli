5d766c5baa0753a0c25bbd4a738a38c800c41c68
Lidlll23 <liva.ievina09@gmail.com>
2025-04-04 08:38:13 +0300

    Add files via upload

---
+++ lifs.py
@@ -0,0 +1,10 @@
+stafsA=int(input("Kurā stāvā tu atrodies?"))
+stafsJ=int(input("Uz kuru stāvu vēlies doties?"))
+
+
+print(stafsA)
+stafsJ=stafsA-(stafsA-stafsJ)
+print(stafsJ)
+
+print("STOP")
+print("Tu esi ieradies izvēlētajā stāvā!")
