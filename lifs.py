a799d2f9d431d5b75b3b35c0deb0becfeebbe5ea
Lidlll23 <liva.ievina09@gmail.com>
2025-04-10 20:45:08 +0300

    Update lifs.py

--- lifs.py
+++ lifs.py
@@ -4,6 +4,18 @@ stafsJ=int(input("Uz kuru stāvu vēlies doties?"))
 
 print(stafsA)
 stafsJ=stafsA-(stafsA-stafsJ)
+
+d1=stafsA-stafsJ
+st=stafsA-1
+
+while d1>=stafsJ:
+    if st>=stafsJ:
+        st2=stafsA-1
+        print(st)
+        print(st2)
+    else:
+        break
+
 print(stafsJ)
 
 print("STOP")
