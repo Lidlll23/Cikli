c11f59ba44982a0a8d348110db0f81dec991cf54
Lidlll23 <liva.ievina09@gmail.com>
2025-04-11 08:39:19 +0300

    Add files via upload

---
+++ MetamaisKaulins.py
@@ -0,0 +1,15 @@
+import random
+skaitlis=random.randrange(1,7) #ikavās norāda (sākuma skaitli, beigu skaitli - neieskaitot)
+
+if skaitlis==1:
+    print("*")
+elif skaitlis==2:
+    print("*\n*")
+elif skaitlis==3:
+    print("*\n*\n")
+elif skaitlis==4:
+    print("**\n**")
+elif skaitlis==5:
+    print("**\n*\n**")
+elif skaitlis==6:
+    print("***\n***")
